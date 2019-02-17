0ddca66d4849550bb902577456a443f15ed4457e
Pedro de Brito <pedro.debrito@wonderkind.com>
2019-02-17 21:26:23 +0100

    docs: Update HealthCheckHandler function comment

--- reblancer_web.go
+++ reblancer_web.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 )
 
-// HealthCheckHandler provides a handler that returns a HTTP 200 response along
-// with a json object indicating liveness.
+// HealthCheckHandler returns a HTTP 200 response along with a json object
+// indicating liveness.
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, `{"alive": true}`)
