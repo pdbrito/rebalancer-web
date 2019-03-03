8fed7f797e563310680ec94e3fec6ea72cbb3d63
Pedro de Brito <pedro.debrito@wonderkind.com>
2019-03-03 23:24:26 +0100

    refactor: make handler methods private

--- http/http.go
+++ http/http.go
@@ -7,15 +7,12 @@ import (
 	"net/http"
 )
 
-// HealthCheckHandler returns a HTTP 200 response along with a json object
-// indicating liveness.
-func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, `{"alive":true}`)
 }
 
-// PricelistHandler returns a handler for the pricelist endpoint
-func PricelistHandler(pricelist rebalancer.Pricelist) http.HandlerFunc {
+func pricelistHandler(pricelist rebalancer.Pricelist) http.HandlerFunc {
 	b, _ := json.Marshal(pricelist)
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
@@ -26,7 +23,7 @@ func PricelistHandler(pricelist rebalancer.Pricelist) http.HandlerFunc {
 // NewServer returns a *http.Server ready to serve our routes over http
 func NewServer(pricelist rebalancer.Pricelist) *http.Server {
 	router := http.NewServeMux()
-	router.HandleFunc("/healthcheck", HealthCheckHandler)
-	router.HandleFunc("/pricelist", PricelistHandler(pricelist))
+	router.HandleFunc("/healthcheck", healthCheckHandler)
+	router.HandleFunc("/pricelist", pricelistHandler(pricelist))
 	return &http.Server{Addr: "localhost:8080", Handler: router}
 }
--- http/http_test.go
+++ http/http_test.go
@@ -10,75 +10,66 @@ import (
 	"testing"
 )
 
-func TestHealthcheckHandler(t *testing.T) {
-	w := httptest.NewRecorder()
-	r, err := http.NewRequest(http.MethodGet, "", nil)
-
-	if err != nil {
-		t.Errorf("http.NewRequest() err = %s, want nil", err)
-	}
-
-	HealthCheckHandler(w, r)
-
-	t.Run("successful response", func(t *testing.T) {
-		t.Run("returns 200 OK", func(t *testing.T) {
-			if status := w.Code; status != http.StatusOK {
-				t.Errorf("handler returned wrong status code: got %v want %v",
-					status, http.StatusOK)
-			}
-		})
-		t.Run("body contains expected json", func(t *testing.T) {
-			want := `{"alive":true}`
-			if w.Body.String() != want {
-				t.Errorf("handler returned wrong body: got '%s' want '%s'",
-					w.Body.String(), want)
-			}
-		})
-	})
-}
-
-func TestPricelistHandler(t *testing.T) {
-	w := httptest.NewRecorder()
-	r, err := http.NewRequest(http.MethodGet, "", nil)
-
-	if err != nil {
-		t.Errorf("http.NewRequest() err = %s, want nil", err)
-	}
-
-	pricelist := rebalancer.Pricelist{
-		"ETH": decimal.NewFromFloat(200),
-		"BTC": decimal.NewFromFloat(5000),
-	}
-
-	handlerFunc := PricelistHandler(pricelist)
-
-	handlerFunc(w, r)
-
-	t.Run("successful response", func(t *testing.T) {
-		t.Run("returns 200 OK", func(t *testing.T) {
-			if status := w.Code; status != http.StatusOK {
-				t.Errorf("handler returned wrong status code: got %v want %v",
-					status, http.StatusOK)
-			}
-		})
-		t.Run("body contains expected json", func(t *testing.T) {
-			want, err := json.Marshal(pricelist)
-			if err != nil {
-				t.Errorf("error marshalling pricelist: %s", err)
-			}
-			if w.Body.String() != string(want) {
-				t.Errorf("handler returned wrong body: got '%s' want '%s'",
-					w.Body.String(), want)
-			}
-		})
-	})
-}
-
 func TestNewServer(t *testing.T) {
 	pricelist := rebalancer.Pricelist{
 		"ETH": decimal.NewFromFloat(200),
 		"BTC": decimal.NewFromFloat(5000),
 	}
 
-	_ = NewServer(pricelist)
+	s := NewServer(pricelist)
+
+	t.Run("handles request to /healtcheck", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/healthcheck", nil)
+
+		s.Handler.ServeHTTP(w, r)
+
+		t.Run("successful response", func(t *testing.T) {
+			t.Run("returns 200 OK", func(t *testing.T) {
+
+				if status := w.Code; status != http.StatusOK {
+					t.Errorf("handler returned wrong status code: got %v want %v",
+						status, http.StatusOK)
+				}
+			})
+			t.Run("body contains expected json", func(t *testing.T) {
+				want := `{"alive":true}`
+				if w.Body.String() != want {
+					t.Errorf("handler returned wrong body: got '%s' want '%s'",
+						w.Body.String(), want)
+				}
+			})
+		})
+	})
+
+	t.Run("handles request to /pricelist", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/pricelist", nil)
+
+		pricelist := rebalancer.Pricelist{
+			"ETH": decimal.NewFromFloat(200),
+			"BTC": decimal.NewFromFloat(5000),
+		}
+
+		s.Handler.ServeHTTP(w, r)
+
+		t.Run("successful response", func(t *testing.T) {
+			t.Run("returns 200 OK", func(t *testing.T) {
+				if status := w.Code; status != http.StatusOK {
+					t.Errorf("handler returned wrong status code: got %v want %v",
+						status, http.StatusOK)
+				}
+			})
+			t.Run("body contains expected json", func(t *testing.T) {
+				want, err := json.Marshal(pricelist)
+				if err != nil {
+					t.Errorf("error marshalling pricelist: %s", err)
+				}
+				if w.Body.String() != string(want) {
+					t.Errorf("handler returned wrong body: got '%s' want '%s'",
+						w.Body.String(), want)
+				}
+			})
+		})
+	})
 }
